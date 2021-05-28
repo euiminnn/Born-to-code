fa2da1382d5e330a186c04cf214fff3e9f53b992
Euimin Chung <jane3079@korea.ac.kr>
2021-05-28 16:49:19 +0900

    push_swap

--- WIP/push_swap/push_swap.c
+++ WIP/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 22:59:07 by echung            #+#    #+#             */
-/*   Updated: 2021/05/27 23:36:58 by echung           ###   ########.fr       */
+/*   Updated: 2021/05/28 16:49:13 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,6 @@ int	main(int argc, char **argv)
 		i++;
 		j++;
 	}
-	for(i = 0; i < 3; i++)
+	for (i = 0; i < 3; i++)
 		printf("arr[%d] = %c\n", i, arr[i]);
 }
