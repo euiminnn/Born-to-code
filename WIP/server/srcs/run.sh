62d9df20b51c92887ea4343569c97926f05194ac
Euimin Chung <jane3079@korea.ac.kr>
2021-07-05 05:05:01 +0900

    ft_server sapzil done?

--- WIP/server/srcs/run.sh
+++ WIP/server/srcs/run.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
+# all will be run in bash
 
-# 아래 모두 bash로 실행
+# Self-signed SSL
+apt-get -y install openssl
+apt-get -y install vim
+openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Gon/CN=localhost" -keyout localhost.dev.key -out localhost.dev.crt
+chmod 600 localhost.dev.crt localhost.dev.key
+mv localhost.dev.crt /etc/ssl/certs/
+mv localhost.dev.key /etc/ssl/private/
 
-chmod 775 /run.sh
-chown -R www-data:www-data /var/www/
-chmod -R 755 /var/www/
+# Nginx
+cp -rp /root/default /etc/nginx/sites-available
+
+# phpMyAdmin
+apt-get install -y wget
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
+mv phpMyAdmin-5.0.2-all-languages /var/www/html/phpmyadmin
+cp -rp /root/config.inc.php /var/www/html/phpmyadmin/
+
+# Database
+service mysql start
+mysql < /var/www/html/phpmyadmin/sql/create_tables.sql -u root --skip-password
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'echung'@'localhost' IDENTIFIED BY 'mypwd' WITH GRANT OPTION" | mysql -u root --skip-password
+
+# Wordpress
+wget https://wordpress.org/latest.tar.gz
+tar -xvf latest.tar.gz
+mv wordpress/ /var/www/html/
+chown -R www-data:www-data /var/www/html/wordpress
+cp -rp /root/wp-config.php /var/www/html/wordpress
+
+service nginx start
+service php7.3-fpm start
+service mysql restart
--- WIP/server/srcs/wp-config.php
+++ WIP/server/srcs/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'echung' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'a' );
+define( 'DB_PASSWORD', 'mypwd' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
