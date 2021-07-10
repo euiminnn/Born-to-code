ade1b412711eb38793973f3632c1cc9fb7e579c9
Euimin Chung <jane3079@korea.ac.kr>
2021-07-10 19:16:15 +0900

    minitalk 20%

--- WIP/minitalk/client.c
+++ WIP/minitalk/client.c
@@ -6,8 +6,17 @@
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 18:55:53 by echung            #+#    #+#             */
-/*   Updated: 2021/07/09 18:55:55 by echung           ###   ########.fr       */
+/*   Updated: 2021/07/10 19:16:07 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	client(int pid, char *str)
+{
+	kill(pid, SIGUSR1);
+	kill(pid, SIGUSR2);
+}
 
+int 	main(int argc, char **argv)
+{
+	client(pid, argv[2]);
+}
--- WIP/minitalk/server.c
+++ WIP/minitalk/server.c
@@ -6,8 +6,17 @@
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 18:55:42 by echung            #+#    #+#             */
-/*   Updated: 2021/07/09 18:55:43 by echung           ###   ########.fr       */
+/*   Updated: 2021/07/10 19:15:00 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+void	handler(int num)
+{
+	ft_putchar_fd(num, 1);
+}
+int		main(void)
+{
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	printf("PID: %d\n", getpid());
+}
