c6537228f0de04b2d558152f2968fbe8f6d8af8d
Euimin Chung <jane3079@korea.ac.kr>
2021-07-13 23:03:06 +0900

    minitalk signal error

--- WIP/minitalk/server.c
+++ WIP/minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 18:55:42 by echung            #+#    #+#             */
-/*   Updated: 2021/07/10 19:18:25 by echung           ###   ########.fr       */
+/*   Updated: 2021/07/13 22:58:48 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@ void	handler(int num)
 {
 	ft_putchar_fd(num, 1);
 }
+
 int		main(void)
 {
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
+	if ((signal(SIGUSR1, handler) == SIG_ERR)
+			|| (signal(SIGUSR2, handler) == SIG_ERR))
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	
 	printf("PID: %d\n", getpid());
 }
