ec82110a6c1327cd291826ef52f870f8507617c5
Euimin Chung <jane3079@korea.ac.kr>
2021-07-18 00:16:41 +0900

    minitalk client 60%

--- WIP/minitalk/client.c
+++ WIP/minitalk/client.c
@@ -6,12 +6,12 @@
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 18:55:53 by echung            #+#    #+#             */
-/*   Updated: 2021/07/12 18:41:08 by echung           ###   ########.fr       */
+/*   Updated: 2021/07/18 00:14:19 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
+/*
 void	send_str(int pid, char *str)
 {
 	int i;
@@ -31,15 +31,64 @@ void	send_str(int pid, char *str)
 		str++;
 	}
 }
+*/
+/*
+void	pid_error(void)
+{
+	write(2, "Please check PID range.\n", 25);
+}
+*/
+/*
+void	send_char(int pid, char *message)
+{
 
-int 	main(int argc, char **argv)
+}
+
+void	send_message(char *pid, char *message)
 {
 	int	pid;
 
+	pid = ft_atoi(pid);
+	while (*message != '\0')
+	{
+		send_char(pid, *message);
+		message++;
+	}
+}
+*/
+void	usage_error(void)
+{
+	write(2, "usage: ./client [PID] [MESSAGE]\n", 32);
+	exit(0);
+}
+
+#include <stdio.h>
+void	my_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		//write(1, "Received SIGUSR1\n", 17);
+		printf("👦🏻 Love you more\n");
+}
+int 	main(int argc, char **argv)
+{
 	if (argc != 3)
-		input_error();
+		usage_error();
+	signal(SIGUSR1, my_handler);
+	printf("PID: %d\n", getpid());
+	while (1)
+	{
+		printf("👧🏻 I love you babe💕\n");
+		sleep(1);
+	}
+	//send_message(argv[1], argv[2]);
+	return (0);
+	/*
+	if (pid < 0 || pid > 32768)
+		pid_error();
+	*/
+	/*
 	if (is_num(argv[1]))
 		pid = ft_atoi(argv[1]);
 	send_str(pid, argv[2]);
-	return (0);
+	*/
 }
--- WIP/minitalk/ft_isdigit.c
+++ WIP/minitalk/ft_isdigit.c
@@ -1,27 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/10 19:16:38 by echung            #+#    #+#             */
-/*   Updated: 2021/07/17 22:32:18 by echung           ###   ########.fr       */
+/*   Created: 2020/12/24 16:34:44 by echung            #+#    #+#             */
+/*   Updated: 2021/01/22 15:39:23 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
+#include "libft.h"
 
-# define MINITALK_H
-
-# include <unistd.h>
-# include <signal.h>
-# include <stdlib.h>
-
-int		ft_atoi(char *str);
-int		ft_isdigit(int c);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
-
-#endif
+int	ft_isdigit(int c)
+{
+	if ('0' <= c && c <= '9')
+		return (1);
+	else
+		return (0);
+}
