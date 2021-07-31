9b83bb02e0c766d79f4bf8176246aeb1f308311d
Euimin Chung <jane3079@korea.ac.kr>
2021-08-01 00:24:58 +0900

    so_long character move

--- WIP/so_long/practice/my/main.c
+++ WIP/so_long/practice/my/main.c
@@ -27,6 +27,15 @@ typedef struct s_param{
 	char	str[3];
 }				t_param;
 
+
+void	*mlx;
+void	*win;
+t_param	param;
+void	*img;
+void	*img_h1;
+void	*img_h2;
+void	*back;
+
 //Only param->x will be used.
 void			param_init(t_param *param)
 {
@@ -37,7 +46,7 @@ void			param_init(t_param *param)
 	param->str[2] = '\0';
 }
 
-int				key_press(int keycode, t_param *param)
+int				key_press(int keycode)
 {
 	static int a = 0;
 /*
@@ -48,57 +57,57 @@ int				key_press(int keycode, t_param *param)
 	if (keycode == KEY_W)//Action when W key pressed
 	{
 		printf("up key pressed\n");
-		param->x++;
+		param.y -= 32;
 	}
 	else if (keycode == KEY_S) //Action when S key pressed
 	{
 		printf("down key pressed\n");
-		param->x--;
+		param.y += 32;
 	}
 	else if (keycode == KEY_D)
 	{
 		printf("front key pressed\n");
-		param->y++;
+		param.x += 32;
 	}
 	else if (keycode == KEY_A)
 	{
 		printf("back key pressed\n");
-		param->y--;
+		param.x -= 32;
 	}
 	else if (keycode == KEY_ESC) //Quit the program when ESC key pressed
 		exit(0);
-	printf("x: %d\n", param->x);
+	printf("x: %d\n", param.x);
+	mlx_put_image_to_window(mlx, win, back, 0, 0);
+	//mlx_put_image_to_window(mlx, win, img, 5, 5);
+	mlx_put_image_to_window(mlx, win, img_h1, 200 + param.x, 200 + param.y);
+	//mlx_put_image_to_window(mlx, win, img_h2, 400, 400);
 	return (0);
 }
 
 int		main(void)
 {
-	void	*mlx;
-	void	*win;
-	t_param	param;
-	void	*img;
-	void	*img_h1;
-	void	*img_h2;
-
 	int		img_width;
 	int 	img_height;
 
 	//initiation
 	mlx = mlx_init();
+	param_init(&param);
 
 	//open window
 	win = mlx_new_window(mlx, 800, 800, "purple_quack");
 	
 	//key press
-	param_init(&param);
-	mlx_hook(win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
+	mlx_hook(win, X_EVENT_KEY_PRESS, 0, &key_press, 0);
 
 	//add image
+	back = mlx_new_image(mlx, 800, 800);
 	img = mlx_xpm_file_to_image(mlx, "../textures/pduck-cutout.xpm", &img_width, &img_height);
 	img_h1 = mlx_xpm_file_to_image(mlx, "../textures/pduck_hurt1-cutout.xpm", &img_width, &img_height);
 	img_h2 = mlx_xpm_file_to_image(mlx, "../textures/pduck_hurt2-cutout.xpm", &img_width, &img_height);
+
+	mlx_put_image_to_window(mlx, win, back, 0, 0);
 	//mlx_put_image_to_window(mlx, win, img, 5, 5);
-	mlx_put_image_to_window(mlx, win, img_h1, 200, 200);
+	mlx_put_image_to_window(mlx, win, img_h1, 200 + param.x, 200 + param.y);
 	//mlx_put_image_to_window(mlx, win, img_h2, 400, 400);
 	
 	mlx_loop(mlx);
