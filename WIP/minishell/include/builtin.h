06c822d2524bd8dbd74f33a0daf25271a3fa8808
Euimin Chung <jane3079@korea.ac.kr>
2021-09-12 16:38:48 +0900

    minishell builins

--- WIP/minishell/include/builtin.h
+++ WIP/minishell/include/builtin.h
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/12 16:26:07 by echung            #+#    #+#             */
-/*   Updated: 2021/09/12 16:31:50 by echung           ###   ########.fr       */
+/*   Created: 2021/09/12 16:31:58 by echung            #+#    #+#             */
+/*   Updated: 2021/09/12 16:32:43 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef BUILTIN_H
+# define BUILTIN_H
 
-void	mshell_echo(int argc, char **argv, char **env);
-void	mshell_cd(int argc, char **argv, char **env);
-void	mshell_pwd(int argc, char **argv, char **env);
+# include <unistd.h>
 
 #endif
--- WIP/minishell/src/core/builtin/mshell_pwd.c
+++ WIP/minishell/src/core/builtin/mshell_pwd.c
@@ -1,20 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   mshell_pwd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/12 16:26:07 by echung            #+#    #+#             */
-/*   Updated: 2021/09/12 16:31:50 by echung           ###   ########.fr       */
+/*   Created: 2021/09/12 16:26:55 by echung            #+#    #+#             */
+/*   Updated: 2021/09/12 16:37:49 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "builtin.h"
+#define PATH_MAX 1000
 
-void	mshell_echo(int argc, char **argv, char **env);
-void	mshell_cd(int argc, char **argv, char **env);
-void	mshell_pwd(int argc, char **argv, char **env);
+void	mshell_pwd(int argc, char **argv, char **env)
+{
+	char cwd[PATH_MAX];
 
-#endif
+   	if (getcwd(cwd, sizeof(cwd)) != NULL)
+       printf("%s\n", cwd);
+}
+
