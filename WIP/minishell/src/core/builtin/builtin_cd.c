83af4ff13969b8e455b2eb1b3bfde83bdffe62bd
Euimin Chung <jane3079@korea.ac.kr>
2021-10-14 21:45:34 +0900

    minishell add semicolon

--- WIP/minishell/src/core/builtin/builtin_cd.c
+++ WIP/minishell/src/core/builtin/builtin_cd.c
@@ -6,17 +6,18 @@
 /*   By: su <su@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 16:23:34 by echung            #+#    #+#             */
-/*   Updated: 2021/10/14 17:24:27 by echung           ###   ########.fr       */
+/*   Updated: 2021/10/14 21:44:23 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core/builtin.h"
+#include <stdio.h>
 
 void	builtin_cd(int argc, char **argv, char **env)
 {
 	(void)env;
 	(void)argc;
 	if (argc == 1)
-		printf("You need a path!\n")
+		printf("You need a path!\n");
 	chdir(argv[1]);
 }
