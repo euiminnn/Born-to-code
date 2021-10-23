6a68f3fc5a36217fb8378e38c3208019bbc21c05
Euimin Chung <jane3079@korea.ac.kr>
2021-10-23 16:33:04 +0900

    test의 Makefile 작성자 수정해주세요

---
+++ WIP/minishell/test/unit/builtin/test_env/test_env.c
@@ -0,0 +1,45 @@
+#include "core/builtin.h"
+#include "debug/debug_utils.h"
+
+t_env *env;
+
+static int get_argc(char **argv)
+{
+    int i;
+
+    i = 0;
+    while (*argv++)
+        i++;
+    return (i);
+}
+
+void test(char *test_name, char *line)
+{
+    int argc;
+    char **argv;
+    int ret;
+
+    printf("----- %s -----\n", test_name);
+    fflush(stdout);
+
+    argv = ft_split(line, ' ');
+    argc = get_argc(argv);
+    ret = builtin_cd(argc, argv, env, 1);
+
+    fflush(stdout);
+    printf("->\n");
+    printf("%d\n", ret);
+    fflush(stdout);
+    ft_free_strings(argv);
+}
+
+int main(int ac, char **av, char **en)
+{
+    env = init_env(en);
+    
+    test("no path", "cd");
+    
+    test("invalid path", "cd nonopath");
+
+    return (0);
+}
