14373a85f41c5beb4130116889bd5ca9c0a5508b
Euimin Chung <jane3079@korea.ac.kr>
2021-10-26 12:08:35 +0900

    exam02 ready

--- exam/exam02/02gnl/get_next_line.c
+++ exam/exam02/02gnl/get_next_line.c
@@ -1,15 +1,19 @@
 #include "get_next_line.h"
 int	get_next_line(char **line)
 {
-	int	index = -1;
+	int	index = 0;
 	int	ret;
 
 	*line = malloc(999999999);
-	while ((ret = read(0, (*line) + (++index), 1)) > 0)
+	while ((ret = read(0, (*line) + (index), 1)) > 0)
 	{
 		if ((*line)[index] == '\n' && ((*line)[index + 1] = '\0'))
 			return (1);
+		index++;
 	}
-	return (ret == -1) ? -1 : 0;
-}		
-
+	if (ret == -1)
+		return (-1);
+	else
+		return (0);
+	//return (ret == -1) ? -1 : 0;
+}
