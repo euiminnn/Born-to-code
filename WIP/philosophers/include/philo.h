8e907692bc354e299ce9634c5ff276368dcc9856
Euimin Chung <jane3079@korea.ac.kr>
2022-01-19 02:22:20 +0900

    feat: saved input into struct named t_args

--- WIP/philosophers/include/philo.h
+++ WIP/philosophers/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 20:41:52 by echung            #+#    #+#             */
-/*   Updated: 2022/01/18 20:44:16 by echung           ###   ########.fr       */
+/*   Updated: 2022/01/19 01:12:01 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 # define PHILO_H
 
 # include <unistd.h>
-
 # include "libft.h"
+
+typedef struct	s_args {
+	int	number_of_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	minimum_eat;
+}	t_args;
 
 #endif
--- WIP/philosophers/srcs/main.c
+++ WIP/philosophers/srcs/main.c
@@ -6,27 +6,30 @@
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 16:56:52 by echung            #+#    #+#             */
-/*   Updated: 2022/01/19 00:14:07 by echung           ###   ########.fr       */
+/*   Updated: 2022/01/19 02:14:29 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <stdio.h>
 
-void	save_with_option(char **argv)
+void	print_t_args(t_args *input)
 {
-	(void)argv;
+	printf("num: %d\n", input->number_of_philos);
+	printf("die: %d\n", input->time_to_die);
+	printf("eat: %d\n", input->time_to_eat);
+	printf("sleep: %d\n", input->time_to_sleep);
+	printf("min: %d\n", input->minimum_eat);
 }
 
-void	save_without_option(char **argv)
+void	init_args(int argc, char **argv, t_args *input)
 {
-	(void)argv;
-}
-
-void	init_args(int argc, char **argv)
-{
-	if (argc == 5)
-		save_without_option(argv);
-	save_with_option(argv);
+	input->number_of_philos = ft_atoi(argv[1]);
+	input->time_to_die = ft_atoi(argv[2]);
+	input->time_to_eat = ft_atoi(argv[3]);
+	input->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		input->minimum_eat = ft_atoi(argv[5]);
 }
 
 #define ERROR 1
@@ -39,9 +42,13 @@ int	error_msg_args(void)
 
 int	main(int argc, char **argv)
 {
+	t_args	input;
+
+	ft_bzero(&input, sizeof(input));
 	if (argc == 5 || argc == 6)
-		init_args(argc, argv);
+		init_args(argc, argv, &input);
 	else
 		return (error_msg_args());
+	print_t_args(&input);
 	return (0);
 }
