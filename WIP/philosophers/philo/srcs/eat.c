4a1399413cf3eb3665475d2b1132613f1a04f424
Euimin Chung <jane3079@korea.ac.kr>
2022-02-11 17:02:03 +0900

    add: every philo has mutex array

--- WIP/philosophers/philo/srcs/eat.c
+++ WIP/philosophers/philo/srcs/eat.c
@@ -14,36 +14,45 @@
 
 void	put_down(t_philo *philo)
 {
-	philo[left_id(philo)].fork_on_table = NOTINUSE;
-	philo[right_id(philo)].fork_on_table = NOTINUSE;
-	pthread_mutex_unlock(&(philo[left_id(philo)].fork));
-	pthread_mutex_unlock(&(philo[right_id(philo)].fork));
+	// philo->fork_on_table[left_id(philo)] = NOTINUSE;
+	// philo->fork_on_table[right_id(philo)] = NOTINUSE;
+	pthread_mutex_unlock(&(philo->fork[left_id(philo)]));
+	pthread_mutex_unlock(&(philo->fork[right_id(philo)]));
 }
 
 void	pick_up(t_philo *philo)
 {
-	pthread_mutex_lock(&(philo[left_id(philo)].fork));
-	pthread_mutex_lock(&(philo[right_id(philo)].fork));
-	while (can_eat(philo) == FALSE)
-	{
-		usleep(5000);
-	}
-	philo[left_id(philo)].fork_on_table = INUSE;
-	philo[right_id(philo)].fork_on_table = INUSE;
-	philo[philo->id].state = EAT;
-}
+	int	now;
 
+	pthread_mutex_lock(&(philo->fork[left_id(philo)]));
+	pthread_mutex_lock(&(philo->fork[right_id(philo)]));
+	// while (can_eat(philo) == FALSE)
+	// {
+	// 	printf("sleep??\n");
+	// 	usleep(5000);
+	// }
+	// philo->fork_on_table[left_id(philo)] = INUSE;
+	now = get_time_in_ms();
+
+	printf("%d %d has taken a fork\n", now - philo->input->clock, philo->id+1);
+	// philo->fork_on_table[right_id(philo)] = INUSE;
+	now = get_time_in_ms();
+
+	printf("%d %d has taken a fork\n", now - philo->input->clock, philo->id+1);
+	philo->state = EAT;
+}
+/*
 int		can_eat(t_philo *philo)
 {
 	// if (philo[philo->id].state == THINK
-	if (philo[left_id(philo)].fork_on_table == NOTINUSE \
-		&& philo[right_id(philo)].fork_on_table == NOTINUSE)
+	if (philo->fork_on_table[left_id(philo)] == NOTINUSE \
+		&& philo->fork_on_table[right_id(philo)] == NOTINUSE)
 	{
 		return (TRUE);
 	}
 	return (FALSE);
 }
-
+*/
 void	eat(t_philo *philo)
 {
 	int	now;
@@ -52,7 +61,7 @@ void	eat(t_philo *philo)
 	pick_up(philo);
 	time = philo->input->time_to_eat;
 	now = get_time_in_ms();
-	printf("%d philo%d is eating\n", now - philo->input->clock, philo->id);
+	printf("%d %d is eating\n", now - philo->input->clock, philo->id+1);
 	usleep(time * 1000);
 	put_down(philo);
 }
