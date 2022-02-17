4b18f03d2d1429a3466c80ccd5334000c40293aa
Euimin Chung <jane3079@korea.ac.kr>
2022-02-17 20:06:06 +0900

    chore: remove comments

--- WIP/miniRT/gaepo-morning-eagles/src/main.c
+++ WIP/miniRT/gaepo-morning-eagles/src/main.c
@@ -11,13 +11,7 @@ int		main(void)
 	int		j;
 	double	u;
 	double	v;
-/*
-	double	r;
-	double	g;
-	double	b;
-	int		canvas_width;
-	int		canvas_height;
-*/
+
 	t_color3	pixel_color;
 	t_canvas	canv;
 	t_camera	cam;
@@ -26,32 +20,18 @@ int		main(void)
 	canv = canvas(400, 300);
 	cam = camera(&canv, point3(0, 0, 0));
 
-	canvas_width = 256;
-	canvas_height = 256;
-
 	printf("P3\n%d %d\n255\n", canv.width, canv.height);
-	j = canvas_height - 1;
+	j = canv.height - 1;
 
 	while (j >= 0)
 	{
 		i = 0;
 		while (i < canv.width)
 		{
-			/*
-			r = (double)i / (canvas_width - 1);
-			g = (double)j / (canvas_height - 1);
-			b = 0.25;
-			printf("%d %d %d\n", (int)(255.999 * r), (int)(255.999 * g), (int)(255.999 * b));
-			*/
 			u = (double)i / (canv.width - 1);
 			v = (double)j / (canv.height - 1);
 			ray = ray_primary(&cam, u, v);
 			pixel_color = ray_color(&ray);
-			/*
-			pixel_color.x = (double)i / (canvas_weight - 1);
-			pixel_color.y = (double)j / (canvas_height - 1);
-			pixel_color.z = 0.25;
-			*/
 			write_color(pixel_color);
 			++i;
 		}
