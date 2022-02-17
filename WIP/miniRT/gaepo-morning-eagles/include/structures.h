a8353f6d0f3c55d151c9af0faea35c02fa14529a
Euimin Chung <jane3079@korea.ac.kr>
2022-02-17 19:00:22 +0900

    add: set camera, canvas, and viewport

--- WIP/miniRT/gaepo-morning-eagles/include/structures.h
+++ WIP/miniRT/gaepo-morning-eagles/include/structures.h
@@ -6,6 +6,7 @@ typedef struct s_vec3 t_point3;
 typedef struct s_vec3 t_color3;
 typedef struct s_ray t_ray;
 typedef struct s_camera t_camera;
+typedef struct s_canvas t_canvas;
 
 struct s_vec3
 {
@@ -19,4 +20,23 @@ struct s_ray
 	t_point3	orig;
 	t_vec3		dir;
 };
+
+struct s_camera
+{
+	t_point3	orig;
+	double		viewport_h;
+	double		viewport_w;
+	t_vec3		horizontal;
+	t_vec3		vertical;
+	double		focal_len;
+	t_point3	left_bottom;
+};
+
+struct	s_canvas
+{
+	int		width;
+	int		height;
+	double	aspect_ratio;
+};
+
 #endif
