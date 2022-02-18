315e8a0892c0cf625a5491dec1cf1130b4116fa7
Euimin Chung <jane3079@korea.ac.kr>
2022-02-18 22:53:03 +0900

    calculate intersection point using quadratic formula

--- WIP/miniRT/gaepo-morning-eagles/include/trace.h
+++ WIP/miniRT/gaepo-morning-eagles/include/trace.h
@@ -8,6 +8,6 @@ t_ray		ray(t_point3 orig, t_vec3 dir);
 t_point3	ray_at(t_ray *ray, double t);
 t_ray		ray_primary(t_camera *cam, double u, double v);
 t_color3	ray_color(t_ray *r, t_sphere *sphere);
-t_bool      hit_sphere(t_sphere *sp, t_ray *ray);
+double      hit_sphere(t_sphere *sp, t_ray *ray);
 
 #endif
--- WIP/miniRT/gaepo-morning-eagles/src/trace/hit/hit_sphere.c
+++ WIP/miniRT/gaepo-morning-eagles/src/trace/hit/hit_sphere.c
@@ -1,7 +1,7 @@
 #include "structures.h"
 #include "utils.h"
 
-t_bool	hit_sphere(t_sphere *sp, t_ray *ray)
+double	hit_sphere(t_sphere *sp, t_ray *ray)
 {
 	t_vec3	oc;
 
@@ -9,12 +9,17 @@ t_bool	hit_sphere(t_sphere *sp, t_ray *ray)
 	double	b;
 	double	c;
 	double	discriminant;
+	double	x;
 
 	oc = vminus(ray->orig, sp->center);
 	a = vdot(ray->dir, ray->dir);
 	b = 2.0 * vdot(oc, ray->dir);
 	c = vdot(oc, oc) - sp->radius2;
 	discriminant = b * b - 4 * a * c;
+	x = (-b - sqrt(discriminant)) / (2.0 * a)
 
-	return (discriminant > 0);
+	if (discriminant < 0)	//실근이 없을 때
+		return (-1.0);
+	else
+		return (x);
 }
