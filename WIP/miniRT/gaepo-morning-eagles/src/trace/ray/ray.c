ebae88f94e19557b66b6347e3acdd42cfc2b7c5d
Euimin Chung <jane3079@korea.ac.kr>
2022-02-18 23:04:33 +0900

    add: try changing color of the sphere using a Normal

--- WIP/miniRT/gaepo-morning-eagles/src/trace/ray/ray.c
+++ WIP/miniRT/gaepo-morning-eagles/src/trace/ray/ray.c
@@ -29,9 +29,14 @@ t_ray		ray_primary(t_camera *cam, double u, double v)
 t_color3	ray_color(t_ray *ray, t_sphere *sphere)
 {
 	double	t;
+	t_vec3	n;
 
-	if (hit_sphere(sphere, ray))
-		return (color3(1, 0, 0));
+	t = hit_sphere(sphere, ray);
+	if (t > 0.0)
+	{
+        n = vunit(vminus(ray_at(ray, t), sphere->center));
+        return (vmult(color3(n.x + 1, n.y + 1, n.z + 1), 0.5));
+	}
 	else
 	{
 		t = 0.5 * (ray->dir.y + 1.0);
