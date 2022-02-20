7dbfa9073030e3f5819b4837b9633b165c2cb5ea
Euimin Chung <jane3079@korea.ac.kr>
2022-02-20 20:33:53 +0900

    fix: struct approach error

--- WIP/miniRT/gaepo-morning-eagles/src/scene/object_create.c
+++ WIP/miniRT/gaepo-morning-eagles/src/scene/object_create.c
@@ -18,8 +18,8 @@ t_sphere    *sphere(t_point3 center, double radius)
 
     if (!(sp = (t_sphere *)malloc(sizeof(t_sphere))))
         return (NULL);
-    sp.center = center;
-    sp.radius = radius;
-    sp.radius2 = radius * radius;
+    sp->center = center;
+    sp->radius = radius;
+    sp->radius2 = radius * radius;
     return (sp);
 }
