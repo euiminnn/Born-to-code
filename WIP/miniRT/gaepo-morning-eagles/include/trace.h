ff971db79dae2471ac293f6a7c0244d066f886ba
Euimin Chung <jane3079@korea.ac.kr>
2022-02-21 19:17:47 +0900

    add phong_lighting function

--- WIP/miniRT/gaepo-morning-eagles/include/trace.h
+++ WIP/miniRT/gaepo-morning-eagles/include/trace.h
@@ -9,6 +9,7 @@ t_point3	ray_at(t_ray *ray, double t);
 t_ray		ray_primary(t_camera *cam, double u, double v);
 t_color3	ray_color(t_scene *scene);
 void        set_face_normal(t_ray *r, t_hit_record *rec);
+t_color3    phong_lighting(t_scene *scene);
 
 t_bool      hit(t_object *obj, t_ray *ray, t_hit_record *rec);
 t_bool      hit_obj(t_object *obj, t_ray *ray, t_hit_record *rec);
---
+++ WIP/miniRT/gaepo-morning-eagles/src/trace/ray/phong_lighting.c
@@ -0,0 +1,18 @@
+#include "trace.h"
+
+t_color3    phong_lighting(t_scene *scene)
+{
+    t_color3    light_color;
+    t_object    *lights;
+
+    light_color = color3(0, 0, 0);
+    lights = scene->light;
+    while (lights)
+    {
+        if (lights->type == LIGHT_POINT)
+            light_color = vplus(light_color, point_light_get(scene, lights->element));
+        lights = lights->next;
+    }
+    light_color = vplus(light_color, scene->ambient);
+    return (vmin(vmult_(light_color, scene->rec.albedo), color3(1, 1, 1)));
+}
